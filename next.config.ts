19d33df5aacb18e775975c56e987f0868ddd3049
infosonetra <info.sonetra@gmail.com>
2026-01-16 23:59:40 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,13 @@ const nextConfig: NextConfig = {
         pathname: '/wp-content/uploads/**',
       },
     ],
+
+    // 🔥 IMPORTANT PERFORMANCE SETTINGS
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [360, 414, 640, 768, 1024, 1280],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
 };
 
 export default nextConfig;
+
